81b9fb4136386e9014bf030ff7b8576267576be6
xauroxs <xauroxs@gmail.com>
2023-12-14 15:08:49 +0300

    create fullName method to unite firstName and lastName

--- 01-getting-started/app.js
+++ 01-getting-started/app.js
@@ -5,6 +5,11 @@ const vm = Vue.createApp({
       lastName: "Doe",
     };
   },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
 }).mount("#app");
 
 setTimeout(() => {
