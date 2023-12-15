a109502572d6f34092f904ce8f12e5846f2d23c0
xauroxs <xauroxs@gmail.com>
2023-12-15 09:47:51 +0300

    replace preventDefault call with an event modifier

--- 01-getting-started/app.js
+++ 01-getting-started/app.js
@@ -18,7 +18,6 @@ const vm = Vue.createApp({
       this.age--;
     },
     updateLastName(msg, event) {
-      event.preventDefault();
       console.log(msg);
       this.lastName = event.target.value;
     },
