77fdff66c83dbac31ca578699a92408e79ee62dd
xauroxs <xauroxs@gmail.com>
2023-12-20 23:37:07 +0300

    add all lifecycle hooks

--- 03-beyond-fundamentals/app.js
+++ 03-beyond-fundamentals/app.js
@@ -4,4 +4,30 @@ const vm = Vue.createApp({
       message: "Hello world!",
     };
   },
-}).mount("#app");
+  beforeCreate() {
+    console.log("beforeCreate() function was called!", this.message);
+  },
+  created() {
+    console.log("created() function was called!", this.message);
+  },
+  beforeMount() {
+    console.log("beforeMount() function was called!", this.$el);
+  },
+  mounted() {
+    console.log("mounted() function was called!", this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate() function was called!");
+  },
+  updated() {
+    console.log("updated() function was called!");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount() function was called!");
+  },
+  unmounted() {
+    console.log("unmounted() function was called!");
+  },
+});
+
+vm.mount("#app");
