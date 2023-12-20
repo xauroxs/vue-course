0c781af07d037c4ba19812f1fc6553d52ff31373
xauroxs <xauroxs@gmail.com>
2023-12-21 00:10:14 +0300

    created first vue component

--- 03-beyond-fundamentals/app.js
+++ 03-beyond-fundamentals/app.js
@@ -30,4 +30,13 @@ const vm = Vue.createApp({
   },
 });
 
+vm.component("hello", {
+  template: `<h1>{{ message }}</h1>`,
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+});
+
 vm.mount("#app");
