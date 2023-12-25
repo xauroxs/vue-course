fb26fae1d71f6acb65af6f8e45f9bbc3c71af2c6
xauroxs <xauroxs@gmail.com>
2023-12-25 23:10:46 +0300

    create component for animation based on js events

--- 05-transitions-and-animations/src/App.vue
+++ 05-transitions-and-animations/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <FadingText></FadingText>
   <ZoomingText></ZoomingText>
+  <FadingAndZoomingText></FadingAndZoomingText>
 </template>
 
 <script>
 import FadingText from "./components/FadingText.vue";
 import ZoomingText from "./components/ZoomingText.vue";
+import FadingAndZoomingText from "./components/FadingAndZoomingText.vue";
 
 export default {
   name: "App",
-  components: { FadingText, ZoomingText },
+  components: { FadingText, ZoomingText, FadingAndZoomingText },
 };
 </script>
---
+++ 05-transitions-and-animations/src/components/FadingAndZoomingText.vue
@@ -0,0 +1,48 @@
+<template>
+  <button type="button" @click="flag = !flag">Toggle animation</button>
+  <Transition
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+  >
+    <h2 v-if="flag">Hello there</h2>
+  </Transition>
+</template>
+
+<script>
+export default {
+  name: "FadingAndZoomingText",
+  data() {
+    return {
+      flag: false,
+    };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter was called.", el);
+    },
+    enter(el, done) {
+      console.log("enter was called.", el);
+      done();
+    },
+    afterEnter(el) {
+      console.log("afterEnter was called.", el);
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave was called.", el);
+    },
+    leave(el, done) {
+      console.log("leave was called.", el);
+      done();
+    },
+    afterLeave(el) {
+      console.log("afterLeave was called.", el);
+    },
+  },
+};
+</script>
+
+<style></style>
