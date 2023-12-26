ca4cf6ef467a53a72b8e3476a31fc9851a7a6651
xauroxs <xauroxs@gmail.com>
2023-12-26 23:54:29 +0300

    create component to animate a list

--- 05-transitions-and-animations/src/App.vue
+++ 05-transitions-and-animations/src/App.vue
@@ -2,15 +2,17 @@
   <FadingText></FadingText>
   <ZoomingText></ZoomingText>
   <FadingAndZoomingText></FadingAndZoomingText>
+  <List></List>
 </template>
 
 <script>
 import FadingText from "./components/FadingText.vue";
 import ZoomingText from "./components/ZoomingText.vue";
 import FadingAndZoomingText from "./components/FadingAndZoomingText.vue";
+import List from "./components/List.vue";
 
 export default {
   name: "App",
-  components: { FadingText, ZoomingText, FadingAndZoomingText },
+  components: { FadingText, ZoomingText, FadingAndZoomingText, List },
 };
 </script>
---
+++ 05-transitions-and-animations/src/components/List.vue
@@ -0,0 +1,65 @@
+<template>
+  <button type="button" @click="addNumber">Add number</button>
+
+  <ul>
+    <TransitionGroup name="fade">
+      <li
+        v-for="(number, index) in numbers"
+        @click="deleteNumber(index)"
+        :key="number"
+      >
+        {{ index }}: {{ number }}
+      </li>
+    </TransitionGroup>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      flag: false,
+      numbers: [1, 2, 3, 4, 5],
+    };
+  },
+  methods: {
+    addNumber() {
+      let num;
+
+      do {
+        num = Math.floor(Math.random() * 100 + 1);
+      } while (this.numbers.includes(num));
+
+      const index = Math.floor(Math.random() * this.numbers.length);
+
+      this.numbers.splice(index, 0, num);
+    },
+    deleteNumber(index) {
+      this.numbers.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.25s linear;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
