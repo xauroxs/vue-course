83271c7a5074db4f06a0b807a51725bc512b7282
xauroxs <xauroxs@gmail.com>
2023-12-27 11:06:27 +0300

    break down app component to questions & result components

--- 06-quiz/src/App.vue
+++ 06-quiz/src/App.vue
@@ -1,29 +1,20 @@
-<script></script>
-
 <template>
   <div class="ctr">
-    <div class="questions-ctr">
-      <div class="progress">
-        <div class="bar"></div>
-        <div class="status">1 out of 3 questions answered</div>
-      </div>
-      <div class="single-question">
-        <div class="question">Sample Question 1</div>
-        <div class="answers">
-          <div class="answer">Sample Answer 1</div>
-          <div class="answer">Sample Answer 2</div>
-          <div class="answer">Sample Answer 3</div>
-          <div class="answer">Sample Answer 4</div>
-        </div>
-      </div>
-    </div>
-    <div class="result">
-      <div class="title">You got sample result 1!</div>
-      <div class="desc">Enter a short description here about the result.</div>
-    </div>
+    <Questions />
+    <Result />
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
+
+<script>
+import Questions from "./components/Questions.vue";
+import Result from "./components/Result.vue";
+
+export default {
+  name: "App",
+  components: { Questions, Result },
+};
+</script>
 
 <style>
 * {
