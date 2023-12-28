21dca6677e9847b41c7889790229716bd67c329d
xauroxs <xauroxs@gmail.com>
2023-12-28 15:30:41 +0300

    create & register plugin for vee-validate lib

--- 07-music-app/src/main.js
+++ 07-music-app/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import VeeValidatePlugin from '@/includes/validation'
+
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VeeValidatePlugin)
 
 app.mount('#app')
